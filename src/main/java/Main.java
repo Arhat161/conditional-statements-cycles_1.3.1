3ff8a2f838d68c0956fe5d1949dbee58c1765520
Arhat161 <Arhat161@users.noreply.github.com>
2022-07-04 13:03:17 +0300

    translate to english

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,18 @@ class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Пожалуйста, укажите ГОД в формате xxxx : ");
+        System.out.println("Please, input YEAR in format xxxx : ");
         int year = scan.nextInt();
 
         if (year % 400 == 0) {
-            System.out.println("Год високосный!");
+            System.out.println("Not a leap year!");
         } else if (year % 100 == 0) {
-            System.out.println("Год НЕ високосный!");
+            System.out.println("Not a leap year!");
         } else {
             if (year % 4 == 0) {
-                System.out.println("Год високосный!");
+                System.out.println("Leap year!");
             } else {
-                System.out.println("Год НЕ високосный!");
+                System.out.println("Not a leap year!");
             }
         }
         scan.close();
